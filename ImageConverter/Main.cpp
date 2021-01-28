f56b6c88d617a9e1ad8d3f47b56fa565eb0f73ff
Eric-Bwr <boewer2003@gmail.com>
2021-01-28 10:49:46 +0100

    Easier to use

--- ImageConverter/Main.cpp
+++ ImageConverter/Main.cpp
@@ -8,6 +8,36 @@ int desiredChannels = 0;
 
 bool isFlip = false;
 
+bool convertImage(char* path, uint8_t* data, int width, int height, int nrComponents){
+    auto toProcess = new std::string;
+    if (data == nullptr) {
+        std::cout << "Failed to load Image from Data\nThis is critical\n";
+        errorCount += 1;
+        system("pause");
+        return false;
+    }else {
+        LibString filename("Output/");
+        filename += getFilename((char*)path);
+        filename = filename.substring(0, filename.findLast("."));
+        filename += ".bif";
+        Binary_buffer binaryBuffer;
+        binary_buffer_create(&binaryBuffer, 5000000);
+        auto widthData = (uint64_t)width;
+        binary_buffer_push64(&binaryBuffer, widthData);
+        auto heightData = (uint64_t)height;
+        binary_buffer_push64(&binaryBuffer, heightData);
+        auto compData = (uint64_t)desiredChannels;
+        binary_buffer_push64(&binaryBuffer, compData);
+        binary_buffer_push_string_with_termination(&binaryBuffer, (char*)data, width * height * desiredChannels);
+        binary_buffer_write_to_file(&binaryBuffer, filename.toString());
+        free(data);
+        binary_buffer_destroy(&binaryBuffer);
+    }
+    toProcess->clear();
+    delete toProcess;
+    return true;
+}
+
 bool convertImage(const char* path){
     std::cout << "Processing " << path << "..." << std::endl;
     int width, height, nrComponents;
@@ -46,16 +76,15 @@ bool convertImage(const char* path){
     return true;
 }
 
-int main(int argc, char *argv[]){
+bool convertFolder(char* path){
     std::vector<std::string> files;
     if(isFlip)
-        files = getFilenames(argv[0], 22);
+        files = getFilenames(path, 22);
     else
-        files = getFilenames(argv[0], 18);
+        files = getFilenames(path, 18);
     if(files.empty()) {
-        std::cout << "Failed to locate Files" << std::endl;
+        std::cout << "Failed to locate Files at" << path << "\n";
         system("pause");
-        return 0;
     }
     std::cout << "Desired Channels: ";
     std::cin >> desiredChannels;
@@ -64,5 +93,9 @@ int main(int argc, char *argv[]){
     }
     std::cout << "Finished with " << errorCount << " Errors." << std::endl;
     system("pause");
-    return 0;
+    return true;
+}
+
+int main(int argc, char *argv[]){
+    convertFolder(argv[0]);
 }
